4c58e1471e8145334440f0704fc8ef52d7751149
Rahul Balla <rballa@purdue.edu>
2018-10-14 12:31:23 -0400

    added library to display graphs

--- app/src/main/java/com/tearvandroid/tearvandroid/AnalyticsFragment.java
+++ app/src/main/java/com/tearvandroid/tearvandroid/AnalyticsFragment.java
@@ -7,11 +7,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class AnalyticsFragment extends Fragment {
+
+    LineGraphSeries <DataPoint> series;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_analytics, container, false);
+
+        double y, x;
+        x = -5.0;
+
+        View view = inflater.inflate(R.layout.fragment_analytics, container, false);
+        GraphView graphView = (GraphView) view.findViewById(R.id.graph);
+        series = new LineGraphSeries<DataPoint>();
+        for (int i = 0; i < 500; i++) {
+            x = x + 0.1;
+            y = Math.sin(x);
+            series.appendData(new DataPoint(x, y), true, 500);
+        }
+
+        graphView.addSeries(series);
+
+        return view;
+
     }
 }
