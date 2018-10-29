60cc356bc809e8de548eb4f3b95004b53584b87e
Dominic Miller <mill2012@purdue.edu>
2018-10-28 20:43:37 -0400

    Pop-Up Activity created with pop up xml, problem in looper on my android studio

--- app/src/main/java/com/tearvandroid/tearvandroid/CarsInfoFragment.java
+++ app/src/main/java/com/tearvandroid/tearvandroid/CarsInfoFragment.java
@@ -1,5 +1,7 @@
 package com.tearvandroid.tearvandroid;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,26 +9,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CarsInfoFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        Button add = (Button) container.findViewById(R.id.add_car_button);
-        add.setOnClickListener(new View.OnClickListener(){
+        View view = inflater.inflate(R.layout.fragment_cars, container, false);
+        Button add = (Button) view.findViewById(R.id.add_car_button);
+        Button signout_button = (Button) view.findViewById(R.id.sign_out_button);
+        add.setOnClickListener(new View.OnClickListener() { //errors all over View, not sure why
             @Override
             public void onClick(View v) {
                 addNewCar();
             }
         });
 
-        return inflater.inflate(R.layout.fragment_cars, container, false);
+        return view; //inflater.inflate(R.layout.fragment_cars, container, false);
     }
 
-    public void addNewCar(){
+    private void addNewCar(){
 
+        Toast.makeText(getActivity(), "debug" , Toast.LENGTH_SHORT).show();
+
+
+        System.out.println("add button clicked");
+        Intent intent = new Intent(getActivity(), PopUpActivity.class);
+        startActivity(intent);
+        //startActivity(new Intent(getActivity(),PopUpActivity.class));
+        //Dom added, Pop Window
     }
-
 
 }
--- app/src/main/java/com/tearvandroid/tearvandroid/activity_addcar.java
+++ app/src/main/java/com/tearvandroid/tearvandroid/activity_addcar.java
@@ -9,5 +9,7 @@ public class activity_addcar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addcar);
+
     }
+
 }
