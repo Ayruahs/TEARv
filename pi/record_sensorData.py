9f0acce2ab4257ae261ab45080528bfc6ade7a2a
Xu He <hexu16461426@gmail.com>
2018-10-31 18:09:33 -0400

    - save sensor data to json - set up endpoint for sensor data

---
+++ pi/record_sensorData.py
@@ -0,0 +1,60 @@
+# Built for Purdue Air Sense Project translated to TEARv (CS 30700 - Team 36)
+
+import time
+import datetime
+import Adafruit_DHT
+import pytz
+import json
+import statistics
+import io
+import uuid
+
+TZ = pytz.timezone("America/New_York")
+
+def main():
+    sensor = Adafruit_DHT.DHT22
+    now = datetime.datetime.now()
+    file = path('/sensor_data/' + now.year + now.month + now.day + '/' +
+                now.year + '-' + now.month + '-' + now.day)
+
+    if not file.isfile():
+        data = {}
+        data['results'] = []
+        data['count'] = 0
+        count = 0
+    else:
+        data = json.load(file, 'r')
+        count = data['count']
+
+        while True:
+            time = datetime.datetime.utcnow().isoformat()
+            humidity, temperature = Adafruit_DHT.read_retry(sensor, 9)
+
+            if humidity is not None and temperature is not None:
+                count = count + 1
+                data['results'].append({
+                    'time': time,
+                    'temperature': round(temperature, 2),
+                    'humidity': round(humidity, 2),
+                    'device': 9,
+                    'id': count
+                })
+
+            else:
+                print('Failed to get reading. Try again!')
+                error = {}
+                error['tempSensor'] = 'error'
+                error['timestamp'] = time
+
+                with open(file+'/error_log', 'a+') as error_log:
+                    json.dump(error, error_log)
+
+            with open(file, 'a+') as file:
+                json.dump(data, file)
+
+            time.sleep(5)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ pi/server/get_data.py
@@ -0,0 +1,35 @@
+import time
+import sys
+import json
+import numpy
+import Adafruit_DHT
+import Tkinter as tk
+
+# Get sensors' data from Pi and send it to the app
+
+def run(day, start, end):
+    now = datetime.datetime.now()
+    file = path('/sensor_data/' + now.year + now.month + now.day + '/' +
+                now.year + '-' + now.month + '-' + now.day)
+
+    if not file.isfile():
+    #return no data
+    else:
+        #check if there is reading error
+        errorfile = path(file+'/error_log')
+        if errorfile.isfile():
+            error = json.load(errorfile, 'r')
+            time = error['tempSensor']['time']
+            if datetime.datetime.utcnow().isoformat() - time < 5:
+                return -1
+            else:
+                # delete error file
+
+        data = json.load(file, 'r')
+        count = data['count']
+
+        for i in range(200):
+            #format json string to return
+
+    return
+
