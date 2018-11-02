43ad4f5e9c9e33c1bb3dc6220f31d04e801fdb5d
Rahul Balla <rballa@purdue.edu>
2018-11-02 16:06:03 -0400

    fixed Cars tab crashing issue

--- app/src/main/java/com/tearvandroid/tearvandroid/CarsInfoFragment.java
+++ app/src/main/java/com/tearvandroid/tearvandroid/CarsInfoFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,21 +17,28 @@ import android.widget.Toast;
 
 public class CarsInfoFragment extends Fragment {
 
-    RelativeLayout carPageLayout;
+//    RelativeLayout carPageLayout;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cars, container, false);
-        Button add = (Button) view.findViewById(R.id.add_car_button);
-        Button signout_button = (Button) view.findViewById(R.id.sign_out_button);
-        add.setOnClickListener(new View.OnClickListener() { //errors all over View, not sure why
+        FloatingActionButton add = view.findViewById(R.id.add_car_button);
+//        Button signout_button = (Button) view.findViewById(R.id.sign_out_button);
+
+        add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 addNewCar();
             }
         });
-        carPageLayout = view.findViewById(R.id.fragment_carsID);
+//        add.setOnClickListener(new View.OnClickListener() { //errors all over View, not sure why
+//            @Override
+//            public void onClick(View v) {
+//                System.out.println("hello");
+//            }
+//        });
+//        carPageLayout = view.findViewById(R.id.fragment_carsID);
 
         return view; //inflater.inflate(R.layout.fragment_cars, container, false);
     }
