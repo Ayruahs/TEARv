8f6ffa486074726b467880d1de6acdfab2895941
Xu He <hexu16461426@gmail.com>
2018-11-01 21:47:47 -0400

    - clean up

--- pi/server/get_data.py
+++ pi/server/get_data.py
@@ -47,27 +47,14 @@ def run(requestId):
     counter = 0
     print data
 
-<<<<<<< HEAD
-    with open('/home/pi/cs307/TEARv/pi/sensor_data/2018-11-1-copy', 'rb+') as filehandle:
-        print filehandle.tell()
+    with open(ffile, 'rb+') as filehandle:
         filehandle.seek(-2, os.SEEK_END)
         last_c = filehandle.read(1)
         print 'last char'
         print last_c
         if last_c == ",":
-            print 'yes'
-            print filehandle.tell()
             filehandle.seek(-2, os.SEEK_END)
             filehandle.write("]")
-=======
-    with open(ffile, 'rb+') as filehandle:
-        filehandle.seek(-2, os.SEEK_END)
-        last_c = filehandle.read(1)
-        print last_c
-        if last_c == ',':
-            filehandle.seek(-2, os.SEEK_END)
-            filehandle.write(']')
->>>>>>> 98f33d137fdc890cd3451a442d473526cd3183c9
 
     with open(ffile) as f1:
         results = json.load(f1)
