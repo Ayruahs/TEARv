01f2a2d3c8e7c463a07a3a867aaae7c1b925c24e
Dominic Miller <mill2012@purdue.edu>
2018-11-30 02:42:53 -0500

    Help page added

--- app/src/main/java/com/tearvandroid/tearvandroid/CarsInfoFragment.java
+++ app/src/main/java/com/tearvandroid/tearvandroid/CarsInfoFragment.java
@@ -1,37 +1,35 @@
+
 package com.tearvandroid.tearvandroid;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class CarsInfoFragment extends Fragment {
 
-//    RelativeLayout carPageLayout;
+    RelativeLayout carPageLayout;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cars, container, false);
-        FloatingActionButton add = view.findViewById(R.id.add_car_button);
+        TextView textView = (TextView) view.findViewById(R.id.help);
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        //FloatingActionButton add = view.findViewById(R.id.add_car_button);
 //        Button signout_button = (Button) view.findViewById(R.id.sign_out_button);
 
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addNewCar();
-            }
-        });
+        //add.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            //public void onClick(View v) {
+             //   addNewCar();
+            //}
+        //});
 //        add.setOnClickListener(new View.OnClickListener() { //errors all over View, not sure why
 //            @Override
 //            public void onClick(View v) {
@@ -43,7 +41,7 @@ public class CarsInfoFragment extends Fragment {
         return view; //inflater.inflate(R.layout.fragment_cars, container, false);
     }
 
-    private void addNewCar(){
+//    private void addNewCar(){
         //carPageLayout.setBackgroundColor(Color.argb(255,255,255,255));
         /*Toast.makeText(getActivity(), "debug" , Toast.LENGTH_SHORT).show();
         System.out.println("add button clicked");
@@ -54,6 +52,6 @@ public class CarsInfoFragment extends Fragment {
         //Dom added, Pop Window
         //carPageLayout
 
-    }
+  //  }
 
 }
